b349309a112215056ef74ef20f07cdab016ab881
sdebarun <sdebarun04@gmail.com>
2023-02-12 01:02:18 +0530

    adding new home template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <footer-component></footer-component>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import FooterComponent from '@/components/FooterComponent.vue'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    FooterComponent
+
   }
 }
 </script>
