0c5fbb71c8a08392bd225e535aa720ad030cf434
sdebarun <sdebarun04@gmail.com>
2023-03-01 10:21:56 +0530

    adding some data with vuetify in front page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,33 @@
 <template>
   <v-container>
-    <h2>Hi, This is my Vue learning app</h2>
-    
+    <h2>{{ subject }}</h2>
+    <v-divider></v-divider>
+    <v-list lines="one">
+      <v-list-item v-for="topic in topics" :key="topic.title" :title="topic.title"><b>{{ topic.title }}</b> - {{ topic.content }}</v-list-item>
+    </v-list>
   </v-container>
 </template>
+
+<script>
+  export default{
+    data() {
+      return {
+        subject: "Hi all, It is my vue learning app",
+        topics: [
+          {
+            title : "Basics of vue js",
+            content : "The basic component structure and parameters"
+          },
+          {
+            title : "Vue router",
+            content : "How routing in vue js works and how it can be used to make SPA"
+          },
+           {
+            title : "Vues Store",
+            content : "to manage the state of the data and use it efficiently to make the life a bit easier"
+          }
+        ]
+      }
+    },
+  }
+</script>
